6674723ef8f74c96e57ebc4456787299f9099b5d
Andrea Hergert <ahergert24@hotmail.com>
2022-11-10 13:37:10 -0800

    took out decimal limitation may put back if needed to display properly

--- models/Product.js
+++ models/Product.js
@@ -21,7 +21,7 @@ Product.init(
       allowNull: false
     },
     price: {
-      type: DataTypes.DECIMAL(10, 2),
+      type: DataTypes.DECIMAL,
       allowNull: false,
       validate: {
         isDecimal: true,
