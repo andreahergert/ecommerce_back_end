6a6ee090bc64a59a4c1d16cef89c8cd3e506af8b
Andrea Hergert <ahergert24@hotmail.com>
2022-11-11 11:55:22 -0800

    changed catagory id to product id in places

--- models/index.js
+++ models/index.js
@@ -6,28 +6,24 @@ const ProductTag = require('./ProductTag');
 
 // Products belongsTo Category
 Product.belongsTo(Category, {
-  foreignKey: 'product_id',
+  foreignKey: 'category_id',
 });
 
 // Categories have many Products
 Category.hasMany(Product, {
-  foreignKey: 'product_id',
+  foreignKey: 'category_id',
 });
 
 // Products belongToMany Tags (through ProductTag)
 Product.belongsToMany(Tag, {
-  through: {
-    model: ProductTag,
+  through: ProductTag,
     foreignKey: 'product_id',
-  },
 });
 
 // Tags belongToMany Products (through ProductTag)
 Tag.belongsToMany(Product, {
-  through: {
-    model: ProductTag,
+    through: ProductTag,
     foreignKey: 'tag_id',
-  },
 });
 
 module.exports = {
