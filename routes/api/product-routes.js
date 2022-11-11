7df9146af22fafe9d48fe5786a9933290788e3ab
Andrea Hergert <ahergert24@hotmail.com>
2022-11-10 17:12:04 -0800

    added some product routes

--- routes/api/product-routes.js
+++ routes/api/product-routes.js
@@ -4,17 +4,66 @@ const { Product, Category, Tag, ProductTag } = require('../../models');
 // The `/api/products` endpoint
 
 // get all products
-router.get('/', (req, res) => {
+router.get('/', async (req, res) => {
   // find all products
   // be sure to include its associated Category and Tag data
+  try {
+    const productData = await Product.findAll({
+      attributes: ['id', 'product_name', 'price', 'stock', 'category_id'],
+      include: [
+      {
+        model: Category,
+        attributes: ['category_name']
+      },
+      {
+        model: Tag,
+        attributes: ['tag_name']
+      }
+    ],
+    });
+
+    if (!productData) {
+      res.status(404).json({ message: 'No products found!' });
+      return;
+    }
+
+    res.status(200).json(productData);
+  } catch (err) {
+    res.status(500).json(err);
+  }
 });
 
 // get one product
-router.get('/:id', (req, res) => {
+router.get('/:id', async (req, res) => {
   // find a single product by its `id`
   // be sure to include its associated Category and Tag data
+  try {
+    const productData = await Product.findByPk(req.params.id,{
+      attributes: ['id', 'product_name', 'price', 'stock', 'category_id'],
+      include: [
+      {
+        model: Category,
+        attributes: ['category_name']
+      },
+      {
+      model: Tag,
+      attributes: ['tag_name']
+      }
+    ],
+    });
+
+    if (!productData) {
+      res.status(404).json({ message: 'No products found!' });
+      return;
+    }
+
+    res.status(200).json(productData);
+  } catch (err) {
+    res.status(500).json(err);
+  }
 });
 
+// Stopped here
 // create new product
 router.post('/', (req, res) => {
   /* req.body should look like this...
